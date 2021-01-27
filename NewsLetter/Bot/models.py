c22fe244257b763290ba0df45b07779eaefc85bc
Vlad <vsakayafignavlad@gmail.com>
2021-01-28 00:46:05 +0300

    Models and migrations

--- NewsLetter/Bot/models.py
+++ NewsLetter/Bot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+
+    user_name = models.CharField("Имя", max_length=50)
+    user_tlg = models.CharField("Имя", max_length=30)
+    expiration_date = models.DateField()
--- NewsLetter/Parser/models.py
+++ NewsLetter/Parser/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
 # Create your models here.
+class Article(models.Model):
+
+    article_title = models.CharField("Название", max_length=100)
+    article_text = models.TextField("Описание")
